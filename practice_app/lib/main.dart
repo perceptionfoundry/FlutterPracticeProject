4bd1e6ca86aada8aa552bd7f36f6951ac4260ae5
Perception Foundry <31928839+perceptionfoundry@users.noreply.github.com>
2022-08-25 13:47:42 +0500

    COMMENTS

--- practice_app/lib/main.dart
+++ practice_app/lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:practice_app/ui/appCollections.dart';
 
 void main() {
+  // FIRST METHOD
   // runApp(const MaterialApp(
   //   home: ScaffoldExample(),
   // ));
-  
+
+  // SECOND METHOD
   runApp(const myBiz());
 }
